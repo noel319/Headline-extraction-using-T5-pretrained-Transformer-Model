25392a38311984a590f32bea64e2b374f070f365
Daniil999464 <goldencoincopper@gmail.com>
2024-05-24 13:30:06 -0700

    update text-tokenization

--- Textprocessing.py
+++ Textprocessing.py
@@ -6,3 +6,22 @@ nltk.download('wordnet') #download wordNet, required for semantic analysis for l
 nltk.download('stopwords') 
 #nltk.download('average_perceptron_tragger') #Download POS tagger
 nltk.download('omw-1.4') #Download the wordnet OMW corpus
+doc1 = pd.read_csv("TextProcessing/doc1.txt")
+doc2 = pd.read_csv("TextProcessing/doc2.txt")
+doc3 = pd.read_csv("TextProcessing/doc3.txt")
+corpus = [doc1, doc2, doc3]
+
+# Tokenization
+
+from nltk.tokenize import word_tokenize
+tokenized_docs=[]
+for doc in corpus:
+    tokens = word_tokenize(str(doc))
+    tokenized_docs.append(tokens)
+
+#printing the all the tokens for all documents and the token lenghts for each document
+print('\nTokenized Documents:')
+for i, doc in enumerate(tokenized_docs):
+    print(f"Docunent {i+1} Tokens:", doc[:], "...(Total:", len(doc), ")")
+    print() # add an empty print statement for a new line
+
