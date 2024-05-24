754b8ffa0d8453f2da9d8be71bee29b1dbdcb874
Daniil999464 <goldencoincopper@gmail.com>
2024-05-24 13:42:30 -0700

    add Lowercase

--- Textprocessing.py
+++ Textprocessing.py
@@ -20,8 +20,22 @@ for doc in corpus:
     tokenized_docs.append(tokens)
 
 #printing the all the tokens for all documents and the token lenghts for each document
-print('\nTokenized Documents:')
-for i, doc in enumerate(tokenized_docs):
-    print(f"Docunent {i+1} Tokens:", doc[:], "...(Total:", len(doc), ")")
-    print() # add an empty print statement for a new line
+# print('\nTokenized Documents:')
+# for i, doc in enumerate(tokenized_docs):
+#     print(f"Docunent {i+1} Tokens:", doc[:], "...(Total:", len(doc), ")")
+#     print() # add an empty print statement for a new line
+
+# Lowercasing
+
+lowercased_docs =[]
+for tokens in tokenized_docs:
+    lowercased_tokens = [token.lower() for token in tokens]
+    lowercased_docs.append(lowercased_tokens)
+
+# Print the tokenized and lowercased documents
+
+print("\nTokenized and Lowercased Documents:")
+for i, doc in enumerate(lowercased_docs):
+    print(f"Document {i+1} Tokens:", doc, "...(Total:", len(doc), ")\n")
+    
 
