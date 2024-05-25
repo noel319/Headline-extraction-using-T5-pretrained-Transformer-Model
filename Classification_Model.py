b517c6b7e9c49ec32354044b66d0c2e89637b753
noel319 <azeisl917@gmail.com>
2024-05-25 13:14:22 -0700

    add dataset

--- Classification_Model.py
+++ Classification_Model.py
@@ -4,7 +4,7 @@ import numpy as np
 import spacy
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
-from sklearn.model_selection import train_test_split, cross_validate, StartifiedKFold
+from sklearn.model_selection import train_test_split, cross_validate, StratifiedKFold
 from sklearn.pipeline import Pipeline
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB, BernoulliNB
@@ -18,3 +18,7 @@ nltk.download('stopwords')
 train = pd.read_csv('Classification/train.csv')
 validation = pd.read_csv('Classification/validation.csv')
 test = pd.read_csv('Classification/test.csv')
+
+# View the train data
+
+print(train.head())
