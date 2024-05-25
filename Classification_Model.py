d798a5a53714520618b90f3fbeb2158811a70511
noel319 <azeisl917@gmail.com>
2024-05-25 13:02:21 -0700

    inital Classification

---
+++ Classification_Model.py
@@ -0,0 +1,20 @@
+import nltk
+import pandas as pd
+import numpy as np
+import spacy
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from sklearn.model_selection import train_test_split, cross_validate, StartifiedKFold
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB, BernoulliNB
+from sklearn.metrics import accuracy_score, classification_report
+
+# Download NLTK resuordes
+nltk.download('punkt')
+nltk.download('stopwords')
+#read the dataset
+
+train = pd.read_csv('Classification/train.csv')
+validation = pd.read_csv('Classification/validation.csv')
+test = pd.read_csv('Classification/test.csv')
