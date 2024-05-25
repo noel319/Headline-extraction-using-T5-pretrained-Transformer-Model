312da32a17aaf6e43ad28480bf9c5e4972b5fdcc
noel319 <azeisl917@gmail.com>
2024-05-25 06:26:16 -0700

    add Lemmatized

--- Textprocessing.py
+++ Textprocessing.py
@@ -88,10 +88,23 @@ for tokens in no_stopwords_including_also_docs:
     stemmed_tokens = [stemmer.stem(token) for token in tokens]
     stemmed_docs.append(stemmed_tokens)
 # Print the stemmed documents, along with their token counts
-print("\nDocuments with Tokens Lowercased, Punctuation Removed, Stopwords Including 'also' Removed, and Stemmed:")
-for i, doc in enumerate(stemmed_docs):
+# print("\nDocuments with Tokens Lowercased, Punctuation Removed, Stopwords Including 'also' Removed, and Stemmed:")
+# for i, doc in enumerate(stemmed_docs):
+#     print(f"Document {i+1} Tokens:", doc, "...(Total:", len(doc), ")\n")
+
+# Lemmatization   
+from nltk.stem import WordNetLemmatizer
+# Initialize the Wordnet Lemmatizer
+lemmatizer = WordNetLemmatizer()
+# Apply lemmatization to the documents
+lemmatized_docs = []
+for tokens in no_stopwords_including_also_docs:
+    lemmatized_tokens = [lemmatizer.lemmatize(token) for token in tokens]
+    lemmatized_docs.append(lemmatized_tokens)
+
+#pring the lemmatized documents, along with their token counts
+print("\nDocuments with Tokens Lowercased, Punctuation Removed, Stopwords Including 'also' Removed, and Lemmatized:")
+for i, doc in enumerate(lemmatized_docs):
     print(f"Document {i+1} Tokens:", doc, "...(Total:", len(doc), ")\n")
-    
-    
 
 
