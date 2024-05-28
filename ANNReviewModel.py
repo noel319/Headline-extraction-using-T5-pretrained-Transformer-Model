ddb7dfb91c736a4668cd2819c6394d3e788f195e
noel319 <azeisl917@gmail.com>
2024-05-28 08:13:07 -0700

    add ANNmodel

---
+++ ANNReviewModel.py
@@ -0,0 +1,115 @@
+#import libraries
+import nltk
+import pandas as pd
+import numpy as np
+import spacy
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from sklearn.model_selection import train_test_split, cross_validate, StratifiedKFold
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB, BernoulliNB
+from sklearn.metrics import accuracy_score, classification_report
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, TensorDataset
+tfidf_ann_vectorizer = TfidfVectorizer(min_df=0.001)
+
+#import data from csv file
+train = pd.read_csv('Classification/train.csv')
+validation = pd.read_csv('Classification/validation.csv')
+test = pd.read_csv('Classification/test.csv')
+
+# Load the spacy Engilsh model
+# Since we are not using NER, we can disable it to speedup
+nlp = spacy.load("en_core_web_sm", disable='ner')
+
+
+#fitting the ann_vectorizer to the train and validation data
+
+features_trains = tfidf_ann_vectorizer.fit_transform(train['user_review'])
+features_validation = tfidf_ann_vectorizer.transform(validation['user_review'])
+
+#converting the feature_train andfeatures-validation
+
+features_trains = torch.tensor(features_trains.toarray(), dtype=torch.float32)
+features_validation = torch.tensor(features_validation.toarray(), dtype = torch.float32)
+
+# convert target variables into pytorch tensors
+y_train = torch.tensor(train['user_suggestion'])
+y_validation = torch.tensor(validation['user_suggestion'])
+
+# Create DataLoader
+# initialise TensorDataset object
+train_dataset = TensorDataset(features_trains, y_train)
+val_dataset = TensorDataset(features_validation, y_validation)
+
+train_loader = DataLoader(train_dataset, batch_size=256, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=256, shuffle=True)
+
+class ANNModel(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size, dropout_rate):
+        super(ANNModel, self).__init__()
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(dropout_rate)
+        self.fc2 = nn.Linear(hidden_size, output_size)
+        self.fc3 = nn.Sigmoid()
+
+    def forward(self, x):
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.dropout(out)
+        out = self.fc2(out)
+        out = self.fc3(out)
+        return out
+
+model = ANNModel(input_size=features_trains.shape[1], hidden_size=64, output_size=1, dropout_rate=0.5)
+criterion = nn.BCELoss()
+optimizer = optim.Adam(model.parameters())
+
+import matplotlib.pyplot as plt
+num_epochs = 30 # Number of epochs
+losses= [] # List to store the averageloss per epoch
+val_losses= []
+
+for epoch in range(num_epochs):
+    model.train() # Set the model to training mode
+    total_loss = 0 #Variable to store the total loss in each epoch
+    total_val_loss = 0 
+    count = 0 # Variable to count the number of batches
+    val_count = 0
+    for inputs, labels in train_loader:
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        outputs = outputs.squeeze()  # Squeeze the output to match the label's shape
+        loss = criterion(outputs, labels.float())  # Ensure labels are float
+        loss.backward()
+        optimizer.step()
+        total_loss += loss.item()
+        count += 1
+    average_loss = total_loss / count  # Calculate average loss for the epoch
+    losses.append(average_loss)  # Append average loss to the list
+       
+    for inputs, labels in val_loader:
+        val_outputs = model(inputs)
+        val_outputs = val_outputs.squeeze()  # Squeeze the output to match the label's shape
+        val_loss = criterion(val_outputs, labels.float())  # Ensure labels are float
+        total_val_loss += val_loss.item()
+        val_count += 1
+    average_val_loss = total_val_loss / val_count  # Calculate average loss for the epoch
+    val_losses.append(average_val_loss)  # Append average loss to the list
+    print(f'Epoch {epoch+1}/{num_epochs}, Loss: {average_loss:.4f}, Val Loss: {average_val_loss:.4f}')
+
+# Plotting the training loss
+plt.figure(figsize=(10,5))
+plt.plot(range(1, num_epochs+1), losses, marker='o', linestyle='-',color='b',label = 'train_loss')
+plt.plot(range(1,num_epochs+1), val_losses, marker='o',linestyle='-',color='b',label = 'val_loss')
+plt.title('Training Loss per Epoch')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.legend()
+plt.grid(True)
+plt.show() 
