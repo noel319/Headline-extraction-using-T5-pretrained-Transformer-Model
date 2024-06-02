d1213f9c37ccec41e6006d956f5350fc23a066e2
noel319 <azeisl917@gmail.com>
2024-06-02 14:19:46 +0300

    add RNNmodel

--- word_embedding.py
+++ word_embedding.py
@@ -66,3 +66,60 @@ def text_to_embeddings(text, word2vec_model, seq_length):
         embeddings = embeddings + zero_padding
 
     return embeddings[:seq_length]
+
+# Prepare data
+def prepare_data(reviews, labels, word2vec_model):
+    X = [text_to_embeddings(review, word2vec_model, 100) for review in reviews]
+    X = [torch.tensor(embeddings, dtype=torch.float32) for embeddings in X]
+    y = torch.tensor(labels, dtype=torch.long)
+    return X
+# Prepare data
+X_train, y_train = prepare_data(train.user_review, train.user_suggestion,
+                    word2vec_model)
+
+X_val, y_val = prepare_data(validation.user_review, validation.user_suggestion,
+                    word2vec_model)
+
+# Define hyperparameters
+input_size = word2vec_model.vector_size
+hidden_size = 128
+output_size = 1
+num_layers = 1
+learning_rate = 0.001
+num_epochs = 30
+batch_size = 64
+dropout_rate = 0.5
+leaky_relu_slope = 0.1
+# Create DataLoader
+train_data = TensorDataset(torch.stack(X_train), y_train)
+train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+# Create DataLoader
+val_data = TensorDataset(torch.stack(X_val), y_val)
+val_loader = DataLoader(val_data, batch_size=batch_size, shuffle=True)
+class SentimentRNN(nn.Module):
+    def __init__(self, input_size, hidden_size, output_size, dropout_rate):
+        super(SentimentRNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        # Basic RNN layer
+        self.rnn = nn.RNN(input_size, hidden_size, num_layers, batch_first=True, dropout=dropout_rate if num_layers > 1 else 0)
+        self.leaky_relu = nn.LeakyReLU()  # Leaky ReLU activation layer
+        self.dropout = nn.Dropout(dropout_rate)
+        self.fc = nn.Linear(hidden_size, output_size)
+        self.sigmoid = nn.Sigmoid()  # Sigmoid activation layer
+
+    def forward(self, x):
+        # Initial hidden state
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(x.device)
+        # RNN output
+        out, _ = self.rnn(x, h0)
+        # Apply Leaky ReLU to the outputs of the RNN layer
+        out = self.leaky_relu(out)
+        out = self.dropout(out)
+        # Get the last sequence output for classification
+        out = out[:, -1, :]
+        # Apply the linear layer for the final output
+        out = self.fc(out)
+        # Apply the sigmoid activation
+        out = self.sigmoid(out)
+        return out
